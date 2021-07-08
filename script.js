abbf455aa731f5dc7498054454edd456387d34a9
Akhtaruzzaman <akhtar01947@gmail.com>
2021-07-08 18:44:29 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var sidebar = (function() {
+    "use strict";
+    $('.sub-menu ul').hide();
+    $(".sub-menu a").click(function () {
+        $(this).parent(".sub-menu").children("ul").slideToggle("100");
+        $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+    });
+})();
